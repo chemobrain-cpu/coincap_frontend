03f83062d66121b01ab703d95a03b4c7664fe83a
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2023-05-02 21:55:02 -0700

    'latest'

--- src/component/userscreencomp/Loader.js
+++ src/component/userscreencomp/Loader.js
@@ -10,7 +10,7 @@ export const Loader = () => {
   let { color} = useSelector(state => state.userAuth)
 
   return (
-    <div className={styles.loader} style={{color:color.background}}>
+    <div className={styles.loader} style={{backgroundColor:color.background}}>
 
       <Spinner size={30} color={'#1652f0'} speed={.5} />
 

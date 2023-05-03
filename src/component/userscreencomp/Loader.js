2cb5b61c1f2d4f8ff106ce3d62038001ec94e478
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2023-05-02 21:41:59 -0700

    'latest'

--- src/component/userscreencomp/Loader.js
+++ src/component/userscreencomp/Loader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Spinner } from "react-activity";
 import "react-activity/dist/library.css";
-import styles from './Loader.module.css'
+import styles from './Loader.module.css';
+import { useDispatch,useSelector } from "react-redux";
 
 
 
 export const Loader = () => {
+  let { color} = useSelector(state => state.userAuth)
 
   return (
-    <div className={styles.loader}>
+    <div className={styles.loader} style={{color:color.background}}>
 
       <Spinner size={30} color={'#1652f0'} speed={.5} />
 
